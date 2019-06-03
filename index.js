8a46ab067f7a8ad189721536689a6b75dbce2cd4
Jihwan Oh <fureweb.com@gmail.com>
2019-06-03 18:03:03 +0900

    수정: 실행 코드 정리

--- index.js
+++ index.js
@@ -8,22 +8,26 @@ const successLogFile = fs.createWriteStream('./success.log', { flags: 'w'})
 const errorLogFile = fs.createWriteStream('./error.log', { flags: 'w' })
 
 const getOutputFile = filePath => 'output/' + makeNewFilePath(filePath)
-
-filePathList.forEach(filePath => {
+const getFilePathObject = filePath => {
   const dclFilePath = `${filePath.split('.')[0]}.dcl`
   const sdclFilePath = `${filePath.split('.')[0]}.sdcl`
 
+  const outputFilePath = getOutputFile(filePath)
   const dclFilePaths = [dclFilePath, getOutputFile(dclFilePath)]
   const sdclFilePaths = [sdclFilePath, getOutputFile(sdclFilePath)]
-  const outputFilePath = getOutputFile(filePath)
 
-  const printResult = (error, stdout, stderr) => {
-    if (error) return errorLogFile.write(`${error}\n`)
+  return { outputFilePath, dclFilePaths, sdclFilePaths}
+}
+const printResult = (error, stdout, stderr) => {
+  if (error) return errorLogFile.write(`${error}\n`)
 
-    // 성공 시 stderr로 출력됨 -_-;; ffmpeg..
-    if (stdout) successLogFile.write(`${stdout}\n`)
-    if (stderr) successLogFile.write(`${stderr}\n`)
-  }
+  // 성공 시 stderr로 출력됨 -_-;; ffmpeg..
+  if (stdout) successLogFile.write(`${stdout}\n`)
+  if (stderr) successLogFile.write(`${stderr}\n`)
+}
+
+filePathList.forEach(filePath => {
+  const {outputFilePath, dclFilePaths, sdclFilePaths} = getFilePathObject(filePath)
 
   // wav파일 다운샘플링 및 dcl 파일 복사 처리
   exec(`ffmpeg -i ${filePath} -ar 16000 ${outputFilePath} -y`, printResult)
