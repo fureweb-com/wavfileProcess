8e3219918806244e57539933f40d741c64806734
Jihwan Oh <fureweb.com@gmail.com>
2019-06-03 16:16:50 +0900

    추가: filePathList.js 파일 임시 작성
    - 예시로 사용될 유효한 파일 목록

---
+++ filePathList.js
@@ -0,0 +1,25 @@
+// 유효한 녹음파일 목록 (path)
+const filePathList = [
+  'tasks/32/1/874_0.wav',
+  'tasks/32/1/874_1.wav',
+  'tasks/32/1/874_2.wav',
+  'tasks/32/1/874_3.wav',
+  'tasks/32/1/874_4.wav',
+  'tasks/32/1/874_5.wav',
+  'tasks/32/1/874_6.wav',
+  'tasks/32/1/874_7.wav',
+  'tasks/32/1/874_8.wav',
+  'tasks/32/1/874_9.wav',
+  'tasks/32/1/939_0.wav',
+  'tasks/32/1/939_1.wav',
+  'tasks/32/1/939_2.wav',
+  'tasks/32/1/939_3.wav',
+  'tasks/32/1/939_4.wav',
+  'tasks/32/1/939_5.wav',
+  'tasks/32/1/939_6.wav',
+  'tasks/32/1/939_7.wav',
+  'tasks/32/1/939_8.wav',
+  'tasks/32/1/939_9.wav',
+]
+
+module.exports = filePathList
