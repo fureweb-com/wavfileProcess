2398be2a8bad7f3ea245b720c7d3b5bea57de83d
Jihwan Oh <fureweb.com@gmail.com>
2019-06-03 16:14:19 +0900

    추가: index.js 파일 작성
    - 16000hz로 다운샘플링 및 dcl, sdcl 파일 복사 처리

---
+++ index.js
@@ -0,0 +1,33 @@
+// ffmpeg 설치 필요
+const fs = require('fs')
+const { exec } = require('child_process')
+const { makeNewFilePath } = require('./words.js')
+const filePathList = require('./filePathList.js')
+
+const successLogFile = fs.createWriteStream('./success.log', { flags: 'w'})
+const errorLogFile = fs.createWriteStream('./error.log', { flags: 'w' })
+
+const getOutputFile = filePath => 'output/' + makeNewFilePath(filePath).split('_').filter((_, i) => i > 0).join('_')
+
+filePathList.forEach(filePath => {
+  
+  const dclFilePath = `${filePath.split('.')[0]}.dcl`
+  const sdclFilePath = `${filePath.split('.')[0]}.sdcl`
+
+  const dclFilePaths = [dclFilePath, getOutputFile(dclFilePath)]
+  const sdclFilePaths = [sdclFilePath, getOutputFile(sdclFilePath)]
+  const outputFilePath = getOutputFile(filePath)
+
+  const printResult = (error, stdout, stderr) => {
+    if (error) return errorLogFile.write(`${error}\n`)
+
+    // 성공 시 stderr로 출력됨 -_-;; ffmpeg..
+    if (stdout) successLogFile.write(`${stdout}\n`)
+    if (stderr) successLogFile.write(`${stderr}\n`)
+  }
+
+  // wav파일 다운샘플링 및 dcl 파일 복사 처리
+  exec(`ffmpeg -i ${filePath} -ar 16000 ${outputFilePath} -y`, printResult)
+  exec(`cp ${dclFilePaths[0]} ${dclFilePaths[1]}`, printResult)
+  exec(`cp ${sdclFilePaths[0]} ${sdclFilePaths[1]}`, printResult)
+})
